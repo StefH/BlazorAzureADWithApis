edf6e210057efa0bc38e0dfdf56afc4b45282e2a
Stef Heyenrath <stef.heyenrath@gmail.com>
2021-02-04 20:51:33 +0100

    scopes

--- BlazorAzureADWithApis/Client/Program.cs
+++ BlazorAzureADWithApis/Client/Program.cs
@@ -1,42 +1,32 @@
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BlazorAzureADWithApis.Client
 {
     public class Program
     {
+        public const string ServiceApiUrl = "https://localhost:44324";
+
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-
-            var u = "https://localhost:44324";
-
-            //builder.Services.AddHttpClient("BlazorAzureADWithApis.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
-            //    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
-
 
             builder.Services.AddHttpClient("BlazorAzureADWithApis.ServerAPI")
                 .AddHttpMessageHandler(sp =>
                 {
                     var handler = sp.GetService<AuthorizationMessageHandler>()
                         .ConfigureHandler(
-                            authorizedUrls: new[] { u },
-                         scopes: new[] { "api://61b557d0-0018-4f72-8a7d-6145a941962d/Application.Access" }
-
+                            authorizedUrls: new[] { ServiceApiUrl },
+                            scopes: new[] { "api://61b557d0-0018-4f72-8a7d-6145a941962d/Application.Access" }
                         );
 
                     return handler;
                 });
-            //     .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
             // Supply HttpClient instances that include access tokens when making requests to the server project
             builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorAzureADWithApis.ServerAPI"));
@@ -45,7 +35,8 @@ namespace BlazorAzureADWithApis.Client
             {
                 builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
 
-               options.ProviderOptions.DefaultAccessTokenScopes.Add("api://9549c3d0-648a-43d8-88be-ae33981eefd5/access_as_user");
+                // Add the 'delegated' scope
+                options.ProviderOptions.DefaultAccessTokenScopes.Add($"api://{options.ProviderOptions.Authentication.ClientId}/access_as_user");
             });
 
             await builder.Build().RunAsync();
