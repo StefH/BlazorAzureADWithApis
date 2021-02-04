88515f2bf23fbbbc08ecd09ca6f0b7626315f22c
Stef Heyenrath <stef.heyenrath@gmail.com>
2021-02-04 21:15:39 +0100

    Add client only

---
+++ BlazorClientOnly/Program.cs
@@ -0,0 +1,46 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorClientOnly
+{
+    public class Program
+    {
+        // public const string ServiceApiUrl = "https://localhost:44324";
+        public const string ServiceApiUrl = "https://localhost:5001";
+
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddHttpClient("BlazorAzureADWithApis.ServerAPI")
+                .AddHttpMessageHandler(sp =>
+                {
+                    var handler = sp.GetService<AuthorizationMessageHandler>()
+                        .ConfigureHandler(
+                            authorizedUrls: new[] { ServiceApiUrl },
+                            scopes: new[] { "api://61b557d0-0018-4f72-8a7d-6145a941962d/Application.Access" }
+                        );
+
+                    return handler;
+                });
+
+            // Supply HttpClient instances that include access tokens when making requests to the server project
+            //builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorAzureADWithApis.ServerAPI"));
+
+            builder.Services.AddMsalAuthentication(options =>
+            {
+                builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+
+                // Add the 'delegated' scope
+                options.ProviderOptions.DefaultAccessTokenScopes.Add($"api://{options.ProviderOptions.Authentication.ClientId}/access_as_user");
+            });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
